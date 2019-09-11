be261ed4cfa315e18ccbb67a84122526cb107c2a
João Paulo Back <joaoback47@gmail.com>
2019-09-11 20:14:41 -0300

    [ADD] Metodos inserir dao

--- app/model/dao/LaunchDAOMySQL.class.php
+++ app/model/dao/LaunchDAOMySQL.class.php
@@ -12,7 +12,34 @@
         const TABLE_NAME = "LAUNCH";
 
         public function insert($launch){
+            try {
+                $sql = ""
+                    . "INSERT INTO " . self::NOME_TABELA . " (flight_number, date, rocket_id, mission_id, 
+                    description, image) "
+                    . "VALUES (:flight_number, :date, :rocket_id, :mission_id, :description, :image)";
+                $pdo = Conexao::conectar();
+                $stmt = $pdo->prepare($sql);
+                
+                $stmt->bindParam(':flight_number', $flightNumber, PDO::PARAM_STR);
+                $stmt->bindParam(':date', $date, PDO::PARAM_STR);
+                $stmt->bindParam(':rocket_id', $rocketId, PDO::PARAM_STR);
+                $stmt->bindParam(':mission_id', $missionId, PDO::PARAM_STR);
+                $stmt->bindParam(':description', $description, PDO::PARAM_STR);
+                $stmt->bindParam(':image', $image, PDO::PARAM_STR);
+                
+                $flightNumber       = $launch->getFlightNumber();
+                $date               = $launch->getDate();
+                $rocketId           = $launch->getRocketId();
+                $missionId          = $launch->getMissionId();
+                $description        = $launch->getDescription();
+                $image              = $launch->getImage();
 
+                return $stmt->execute();
+            }catch (PDOException $e){
+                echo 'Erro ao Inserir -> ' . $e->getMessage();
+            }finally{
+                $pdo = null;
+            }
         }
 
         public function update($launch){
@@ -24,7 +51,7 @@
         }
         
         public function find($launch){
-        
+
         }
         
         public function findAll(){
--- app/model/dao/RocketDAOMySQL.class.php
+++ app/model/dao/RocketDAOMySQL.class.php
@@ -12,7 +12,39 @@
         const TABLE_NAME = "ROCKET";
 
         public function insert($rocket){
+            try {
+                $sql = ""
+                    . "INSERT INTO " . self::NOME_TABELA . " (rocket_id, name, description, first_flight, 
+                    height, diameter, mass, image) "
+                    . "VALUES (:rocket_id, :name, :description, :first_flight, :height,
+                    :diameter, :mass, :image)";
+                $pdo = Conexao::conectar();
+                $stmt = $pdo->prepare($sql);
+                
+                $stmt->bindParam(':rocket_id', $rocketId, PDO::PARAM_STR);
+                $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+                $stmt->bindParam(':description', $description, PDO::PARAM_STR);
+                $stmt->bindParam(':first_flight', $firstFlight, PDO::PARAM_STR);
+                $stmt->bindParam(':height', $height, PDO::PARAM_STR);
+                $stmt->bindParam(':diameter', $diameter, PDO::PARAM_STR);
+                $stmt->bindParam(':mass', $mass, PDO::PARAM_STR);
+                $stmt->bindParam(':image', $image, PDO::PARAM_STR);
+                
+                $rocketId       = $rocket->getRocketId();
+                $name           = $rocket->getName();
+                $description    = $rocket->getDescription();
+                $firstFlight    = $rocket->getFirstFlight();
+                $height         = $rocket->getHeight();
+                $diameter       = $rocket->getDiameter();
+                $mass           = $rocket->getMass();
+                $image          = $rocket->getImage();
 
+                return $stmt->execute();
+            }catch (PDOException $e){
+                echo 'Erro ao Inserir -> ' . $e->getMessage();
+            }finally{
+                $pdo = null;
+            }
         }
 
         public function update($rocket){
@@ -24,7 +56,35 @@
         }
         
         public function find($rocket){
-        
+            try{
+                $pdo        = Conexao::conectar();
+                $sql        = 'SELECT * FROM ' . self::NOME_TABELA . ' WHERE id = :id';
+                $stmt      = $pdo->prepare($sql);
+    
+                $stmt->bindParam(':id', $id, PDO::PARAM_STR);
+                $id = $rocket->getId();
+    
+                $stmt->execute();
+    
+                while ($r = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    $rocket = new Rocket();
+                    $rocket->setRocketId($r['rocketId']);
+                    $rocket->setName($r['name']);
+                    $rocket->setDescription($r['description']);
+                    $rocket->setFirstFlight($r['firstFlight']);
+                    $rocket->setHeight($r['height']);
+                    $rocket->setDiameter($r['diameter']);
+                    $rocket->setMass($r['mass']);
+                    $rocket->setImage($r['image']);
+                }
+    
+           
+               return $rocket;
+            }catch (PDOException $e){
+                echo 'Erro ao Listar -> ' . $e->getMessage();
+            }finally{
+                $pdo = null;
+            }
         }
         
         public function findAll(){
